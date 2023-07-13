2105c7319d4aceb8b4360616f8ae90b96283aa23
Thesecondalthowcool <125369193+Thesecondalthowcool@users.noreply.github.com>
2023-07-13 16:35:07 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,1 +1,10 @@
-a
+self.__uv$config = {
+    prefix: '/uv/service/',
+    bare: 'https://tiw.tiwweb.workers.dev/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
